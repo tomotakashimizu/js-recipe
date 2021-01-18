5085bd609e6921e0774392956646bd046b9486a8
tomotakashimizu <tomotaka.developer@gmail.com>
2021-01-18 21:12:37 +0900

    FizzBuzzのコード作成

---
+++ 1_workspace/1/3の倍数/advanced/FizzBuzz/main.js
@@ -0,0 +1,16 @@
+const fizzBuzz = function(number) {
+  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+  for (let n = 1; n <= number; n++) {
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 == 0) {
+      console.log("Fizz")
+    } else if (n % 5 == 0) {
+      console.log("Buzz")
+    } else {
+      console.log(n)
+    }
+  }
+}
+
+fizzBuzz(100)
