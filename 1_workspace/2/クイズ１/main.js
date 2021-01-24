44646ea557d4a3f402aca28e71f7c0a2eca84fe9
tomotakashimizu <tomotaka.developer@gmail.com>
2021-01-24 13:19:20 +0900

    STEP3 - フィードバックと正解判定をわける

--- 1_workspace/2/クイズ１/main.js
+++ 1_workspace/2/クイズ１/main.js
@@ -3,18 +3,17 @@ const choice2 = document.getElementById("choice-2")
 const choice3 = document.getElementById("choice-3")
 const feedback = document.getElementById("feedback")
 
-// STEP2 - 選択肢と選択をわける
+// STEP3 - フィードバックと正解判定をわける
+// feedback の内容
+const feedbacks = [
+  "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+  "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+  "正解！ガニメデは、木星の第三惑星だよ！",
+]
+
 // 正解を判定する
-const choose = function(choiceString) {
-  if (choiceString === 0) {
-    feedback.textContent =
-      "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
-  } else if (choiceString === 1) {
-    feedback.textContent =
-      "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
-  } else {
-    feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
-  }
+const choose = function(choiceNumber) {
+  feedback.textContent = feedbacks[choiceNumber]
 }
 
 choice1.onclick = function() {
@@ -30,6 +29,33 @@ choice3.onclick = function() {
   choose(2)
 }
 
+// STEP2 - 選択肢と選択をわける
+// // 正解を判定する
+// const choose = function(choiceString) {
+//   if (choiceString === 0) {
+//     feedback.textContent =
+//       "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
+//   } else if (choiceString === 1) {
+//     feedback.textContent =
+//       "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
+//   } else {
+//     feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
+//   }
+// }
+//
+// choice1.onclick = function() {
+//   // 0 番目の選択肢を選択
+//   choose(0)
+// }
+// choice2.onclick = function() {
+//   // 1 番目の選択肢を選択
+//   choose(1)
+// }
+// choice3.onclick = function() {
+//   // 2 番目の選択肢を選択
+//   choose(2)
+// }
+
 // STEP1 - 選択肢と正解判定をわける
 // // 正解を判定する
 // const choose = function(choiceString) {
@@ -43,6 +69,7 @@ choice3.onclick = function() {
 //     feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
 //   }
 // }
+//
 // choice1.onclick = function() {
 //   // ゴリアテを選択
 //   choose("ゴリアテ")
