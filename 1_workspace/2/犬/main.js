56774d96929ca22d5e198584b5ead465447489ed
tomotakashimizu <tomotaka.developer@gmail.com>
2021-01-24 13:55:04 +0900

    DogAPIで非同期処理

--- 1_workspace/2/犬/main.js
+++ 1_workspace/2/犬/main.js
@@ -1,10 +1,18 @@
 const imageElement = document.getElementById("dog-image")
 
+console.log("fetchの前")
+
 // 指定したサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch("https://dog.ceo/api/breeds/image/random") // まず fetch でデータの取得をしにいき、
   .then((res) => {
-    return res.json() // 結果を json として読み込む
+    // fetch がおわったら１つめの then の中の関数を実行し、
+    console.log("1つ目")
+    return res.json() // 結果を json として読み込んで、次の then に渡す
   })
   .then((data) => {
+    // １つめが終わったら２つめの then の中の関数を実行します。
+    console.log("2つ目")
     imageElement.src = data.message // 画像を表示する
   })
+
+console.log("fetchの後")
