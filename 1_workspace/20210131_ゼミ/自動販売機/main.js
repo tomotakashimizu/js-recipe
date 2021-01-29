4559c2b684062c387a0debc5e09ef3d47b119ba5
tomotakashimizu <tomotaka.developer@gmail.com>
2021-01-29 17:56:48 +0900

    20210131 のゼミのファイル作成

---
+++ 1_workspace/20210131_ゼミ/自動販売機/main.js
@@ -0,0 +1,30 @@
+const tenButton = document.getElementById("ten-button")
+const fiftyButton = document.getElementById("fifty-button")
+const hundredButton = document.getElementById("hundred-button")
+
+const teaButton = document.getElementById("tea-button")
+const coffeeButton = document.getElementById("coffee-button")
+
+const changeButton = document.getElementById("change-button")
+
+const teaNum = document.getElementById("tea-number")
+const coffeeNum = document.getElementById("coffee-number")
+
+const displayMoney = document.getElementById("display-money")
+const diplayChangeMoney = document.getElementById("change-money")
+
+let totalMoney = 0
+let changeMoney = 0
+
+teaButton.onclick = function() {}
+
+coffeeButton.onclick = function() {}
+
+// const input = document.getElementById("input")
+// const button = document.getElementById("button")
+
+// let money = 0
+
+// button.onclick = function() {
+//   money += Number(input.value)
+// }
