ddf7ef650aced7bb7585d2c021895132fcf181b0
tomotakashimizu <tomotaka.developer@gmail.com>
2021-01-29 17:56:48 +0900

    一時的にコメントアウト

--- 1_workspace/20210131_ゼミ/自動販売機/main.js
+++ 1_workspace/20210131_ゼミ/自動販売機/main.js
@@ -1,24 +1,24 @@
-const tenButton = document.getElementById("ten-button")
-const fiftyButton = document.getElementById("fifty-button")
-const hundredButton = document.getElementById("hundred-button")
+// const tenButton = document.getElementById("ten-button")
+// const fiftyButton = document.getElementById("fifty-button")
+// const hundredButton = document.getElementById("hundred-button")
 
-const teaButton = document.getElementById("tea-button")
-const coffeeButton = document.getElementById("coffee-button")
+// const teaButton = document.getElementById("tea-button")
+// const coffeeButton = document.getElementById("coffee-button")
 
-const changeButton = document.getElementById("change-button")
+// const changeButton = document.getElementById("change-button")
 
-const teaNum = document.getElementById("tea-number")
-const coffeeNum = document.getElementById("coffee-number")
+// const teaNum = document.getElementById("tea-number")
+// const coffeeNum = document.getElementById("coffee-number")
 
-const displayMoney = document.getElementById("display-money")
-const diplayChangeMoney = document.getElementById("change-money")
+// const displayMoney = document.getElementById("display-money")
+// const diplayChangeMoney = document.getElementById("change-money")
 
-let totalMoney = 0
-let changeMoney = 0
+// let totalMoney = 0
+// let changeMoney = 0
 
-teaButton.onclick = function() {}
+// teaButton.onclick = function() {}
 
-coffeeButton.onclick = function() {}
+// coffeeButton.onclick = function() {}
 
 // const input = document.getElementById("input")
 // const button = document.getElementById("button")
